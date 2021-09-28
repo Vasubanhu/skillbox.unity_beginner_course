524d37dd5555da904efd39b85a72343eb5b842d8
Leonid Bedrikov <shangrila1721@gmail.com>
2021-09-28 19:52:33 +0500

    Initial commit

---
+++ exercise_6/Assets/Scripts/Burglar/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Text _timerText;
+    [SerializeField] private float _timeRemaining;
+    [SerializeField] private GameObject[] panels;
+    private Breaking _instance;
+
+    private void Start()
+    {      
+        _instance = FindObjectOfType<Breaking>();
+        Pause();
+    }
+
+    private void Update()
+    {
+        DisplayTimer();
+        GameOver();
+    }
+
+    private void SetTimer() => _timeRemaining = 60.0f;
+
+    private void Pause() => Time.timeScale = 0;
+
+    private void Resume() => Time.timeScale = 1;
+
+    private void DisplayTimer()
+    {
+        if (_timeRemaining > 0)
+        {
+            _timeRemaining -= Time.deltaTime;
+            _timerText.text = Mathf.Round(_timeRemaining).ToString();
+        }
+    }
+
+    private void GameOver()
+    {
+        Win();
+        Lose();
+    }
+
+    private void Win()
+    {
+        string result = $"{_instance.Pin1Value}{_instance.Pin2Value}{_instance.Pin3Value}";
+
+        if (result == _instance._rightCombination)
+        {
+            panels[2].SetActive(true);
+            panels[1].SetActive(false);
+            Pause();
+        }
+    }
+
+    private void Lose()
+    {
+        if (_timeRemaining <= 0)
+        {
+            panels[3].SetActive(true);
+            panels[1].SetActive(false);
+            Pause();
+        }      
+    }
+
+    public void Restart()
+    {
+        SetTimer();
+        panels[1].SetActive(true);
+        panels[2].SetActive(false);
+        panels[3].SetActive(false);
+        _instance.ResetToDefault();
+        Resume();
+    }
+
+    public void StartGame()
+    {
+        SetTimer();
+        panels[1].SetActive(true);
+        panels[0].SetActive(false);
+        _instance.ResetToDefault();
+        Resume();
+    }
+}
---
+++ exercise_6/Assets/Scripts/TimeHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeHandler : MonoBehaviour
+{
+    [SerializeField] private Text _timeText;
+    [SerializeField] private Text _lapCountText;
+    [SerializeField] private Text _lapTimeText;
+    [SerializeField] private Text _previousLapTimeText;
+
+    private List<float> numbers = new List<float>() { 7.7f, 1.4f, 5.5f, 6.5f, -0.5f };
+    private float _currentTime;
+    private float _lapsCount;
+    private float _currentLapTime;
+    private float _previousLapTime;
+    private bool _startRace;
+
+    private void Start()
+    {
+        foreach (float number in numbers)
+        {
+            Debug.Log(Mathf.Round(number));
+        }   
+    }
+
+    private void Update()
+    {
+        Time.timeScale = 0;
+
+        if (_startRace)
+        {
+            Time.timeScale = 1;
+
+            _currentTime = Mathf.Round(Time.time);
+            _timeText.text = _currentTime.ToString();
+        }      
+    }
+
+    public void RoundPassed()
+    {
+        CalculateRaceData();
+        DisplayRaceData();        
+    }
+    public void StartRace()
+    {
+        _startRace = true;       
+    }
+
+    private void CalculateRaceData()
+    {
+        //_previousLapTime = _currentLapTime;
+        _currentLapTime = _currentTime;
+
+        _previousLapTime = _currentLapTime - _previousLapTime; // 
+    }
+    private void DisplayRaceData()
+    {
+        _lapTimeText.text = _currentLapTime.ToString();
+        _previousLapTimeText.text = _previousLapTime.ToString();
+        _lapCountText.text = (++_lapsCount).ToString();
+    }
+}
