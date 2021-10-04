034981c959ec8f85082cb3f3b850f095860a9be7
Leonid Bedrikov <shangrila1721@gmail.com>
2021-10-04 20:04:09 +0500

    initial commit

---
+++ exercise_7/Assets/Scripts/Sound.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    [HideInInspector] public AudioSource AudioSource;
+    [Range(0f, 1f)] public float Volume;
+    [Range(.1f, 1f)] public float Pitch;
+    public string Name;
+    public AudioClip Clip;   
+    public bool Loop;
+}
