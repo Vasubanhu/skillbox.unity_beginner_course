1913ad6f54d28192be7f05e8a0d8002fb90b85ab
Leonid Bedrikov <shangrila1721@gmail.com>
2021-10-09 20:24:46 +0500

    initial commit

---
+++ exercise_8/Assets/Scripts/Move.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform[] _points;
+    [SerializeField] private GameObject _prope;
+    private Vector3 _target;
+    private int _index;
+    private float _angle = 3.0f;
+
+    private void Start() => _target = _points[_index].position;
+
+    private void Update()
+    {
+        LookAt(_target);
+        MoveTo(_target);
+
+        if (CheckPoint(_target))
+        {
+            _index++;
+            _target = _points[_index].position;
+
+            if (_index == _points.Length - 1)
+            {
+                _index = -1;
+            }
+        }
+
+        RotatePrope();
+    }
+
+    private void RotatePrope() => _prope.transform.Rotate(Vector3.down, _angle);
+
+    private void MoveTo(Vector3 target)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, _speed * Time.deltaTime);
+    }
+
+    private bool CheckPoint(Vector3 target)
+    {
+        return (transform.position == target) ? true : false;
+    }
+
+    private void LookAt(Vector3 target)
+    {
+        transform.LookAt(target);
+    }
+}
---
+++ exercise_8/Assets/Scripts/Running/RelayRace.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class RelayRace : MonoBehaviour
+{
+    [SerializeField] private Transform[] _runners;
+    [SerializeField] private float _speed;
+    private float _passDistance = 2.0f;
+    private int _currentRunner;
+    private int _nextRunner;
+    private int _stickIndex;
+    private RelayRace _currentRunnerComponent;
+    private RelayRace _nextRunnerComponent;
+
+    private void Start()
+    {
+        _nextRunner = _currentRunner + 1;
+        _currentRunnerComponent = _runners[_currentRunner].GetComponent<RelayRace>();
+        _nextRunnerComponent = _runners[_nextRunner].GetComponent<RelayRace>();
+    }
+
+    private void Update()
+    {
+        float _distance = Vector3.Magnitude(_runners[_nextRunner].position - _runners[_currentRunner].position);
+
+        LookAt(_runners[_nextRunner]);
+        MoveTo(_runners[_nextRunner]);
+
+        if (CheckDistance(_distance))
+        {
+            _nextRunnerComponent.enabled = true;
+            _currentRunnerComponent.enabled = false;
+            PassBaton();
+        }
+    }
+
+    private void LookAt(Transform target)
+    {
+        transform.LookAt(target);
+    }
+
+    private void MoveTo(Transform target)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+    }
+
+    private bool CheckDistance(float distance)
+    {
+        return (distance <= _passDistance) ? true : false;
+    }
+
+    private void PassBaton()
+    {
+        _runners[_nextRunner].GetChild(_stickIndex).gameObject.SetActive(true);
+        _runners[_currentRunner].GetChild(_stickIndex).gameObject.SetActive(false);
+    }
+}
